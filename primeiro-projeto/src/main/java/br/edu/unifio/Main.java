466bf9675a696466aea32a60224fd1d8ab5d9745
sergiodelfino <88910873+sergiodelfino@users.noreply.github.com>
2025-08-18 23:34:40 +0000

    Exemplo de Projeto com Maven

---
+++ primeiro-projeto/src/main/java/br/edu/unifio/Main.java
@@ -0,0 +1,13 @@
+package br.edu.unifio;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+        String senha = RandomStringUtils.insecure().nextAlphabetic(8);
+        System.out.println(senha);
+    }
+}
