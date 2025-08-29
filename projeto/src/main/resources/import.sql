e074e84c72e98238184f840b7857783218f7c70a
sergiodelfino <88910873+sergiodelfino@users.noreply.github.com>
2025-08-29 00:44:56 +0000

    Aula do dia 01/09/2025

---
+++ projeto/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into Categoria (nome) values ('Frutas');
+insert into Categoria (nome) values ('Bebidas');
+insert into Categoria (nome) values ('Carnes');
+
+insert into Fabricante (nome) values ('The Coca-Cola Company');
+
+insert into Produto (nome, preco, quantidade, validade, categoria_codigo, fabricante_codigo) values ('Coca Cola 2 Litros', 12.00, 15, '2025-12-20', 2, 1);
