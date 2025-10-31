27956457a0ece17b2eaf87ccfd577e88b39528e1
sergiodelfino <88910873+sergiodelfino@users.noreply.github.com>
2025-10-31 00:41:42 +0000

    Aula do Dia 20/10/2025

--- projeto/src/main/java/recursos/ProdutoRecurso.java
+++ projeto/src/main/java/recursos/ProdutoRecurso.java
@@ -3,7 +3,9 @@ package recursos;
 import java.util.List;
 
 import entidades.Produto;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 @Path("produtos")
@@ -11,5 +13,11 @@ public class ProdutoRecurso {
     @GET
     public List<Produto> listar(){
         return Produto.listAll();
+    }
+
+    @POST
+    @Transactional
+    public void salvar(Produto produto) {
+        produto.persist();
     }
 }
